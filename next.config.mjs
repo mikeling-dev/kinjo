030309679f55d7f7bfc6b7fee735752aed47cdaa
Mike Ling <mikeling.dev@gmail.com>
2025-03-13 19:46:43 +0800

    installed dependencies, created navbar with logo, search bar and login button

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; img-src 'self' data: https:; style-src 'self' 'unsafe-inline';",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
